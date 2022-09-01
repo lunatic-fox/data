9a89663a5c74a50a1a09e797c96e9883c4995b3f
unknown <joseliojrx25@gmail.com>
2022-09-01 14:35:08 -0300

    Update buildTable.js

--- src/buildTable.js
+++ src/buildTable.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-/** @param {{query: { t : string}}} req */
+/** @param {{query: { t : string? }}} req */
 const buildTable = async req => {
   const { t } = req.query;
+
+  if (!t) {
+    const res = {
+      t: fs.readdirSync(path.join(__dirname, `../res`))
+        .map(e => e.replace(/(.+)\..+/, '$1'))
+    };
+    return Promise.resolve(JSON.stringify(res, null, 2));
+  }
+
   const uri = path.join(__dirname, `../res/${t}.json`);
 
   if (fs.existsSync(uri))
