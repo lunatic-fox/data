0c451702a7d47fef7bab1dc7bf41dfd07899cd8e
unknown <joseliojrx25@gmail.com>
2022-09-02 16:49:31 -0300

    Update dataTables.js

--- api/dataTables.js
+++ api/dataTables.js
@@ -3,11 +3,11 @@ const buildTable = require('../src/buildTable');
 module.exports = async (req, res) => {
   buildTable(req)
     .then(data => {
-      res.setHeader('Content-Type', 'application/json');
+      res.setHeader('Content-Type', 'text/plain;charset=utf8');
       res.end(data);
     })
     .catch(err => {
-      res.setHeader('Content-Type', 'application/json');
+      res.setHeader('Content-Type', 'text/plain;charset=utf8');
       res.end(err);
     });
 };
